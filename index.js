418100ce73379bb51f757f86ea3f35d20e3b8365
Witsudi Anasrullah <witsudi217@gmail.com>
2020-09-21 19:49:13 +0700

    my apis II

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
