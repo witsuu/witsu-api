b982fd032d077f6b2304ecf4891b57c172200729
Witsudi Anasrullah <witsudi217@gmail.com>
2020-10-09 14:15:01 +0700

    change last maybe

---
+++ validation/userValidation.js
@@ -0,0 +1,27 @@
+const Joi = require("@hapi/joi");
+
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    nama: Joi.string().min(3).required(),
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "id"] },
+    }),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).min(6),
+  });
+  return schema.validate(data);
+};
+
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "id"] },
+    }),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).min(6),
+  });
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
